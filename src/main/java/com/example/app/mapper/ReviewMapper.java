7ccdb5e5e2a6f9a95a1f3ebe349bb2387762b31d
zd2L10 <airrideship556@gmail.com>
2022-10-20 13:25:57 +0900

    ReviewMapperとxml作成

---
+++ src/main/java/com/example/app/mapper/ReviewMapper.java
@@ -0,0 +1,26 @@
+package com.example.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.app.domain.Review;
+
+@Mapper
+public interface ReviewMapper {
+
+	List<Review> selectAll() throws Exception;
+	
+	List<Review> selectByName(String reviewer) throws Exception;
+	
+	void insert(Review review) throws Exception;
+	
+	void update(Review review) throws Exception;
+	
+	void delete(Integer id) throws Exception;
+	
+	Long countReview() throws Exception;
+	
+	List<Review> selectLimited(@Param("offset") int offset, @Param("limit") int limit) throws Exception;
+}
