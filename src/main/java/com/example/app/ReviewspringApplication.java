7f88717f5b971baf5cb49e6314778930fa5f35db
zd2L10 <airrideship556@gmail.com>
2022-10-20 11:44:17 +0900

    初期状態

---
+++ src/main/java/com/example/app/ReviewspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewspringApplication.class, args);
+	}
+
+}
