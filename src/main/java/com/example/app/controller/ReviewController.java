ca708932696941d3b6497a99e7aebf6041bf35bc
zd2L10 <airrideship556@gmail.com>
2022-10-21 14:18:30 +0900

    Controllerをページネーション用に修正

--- src/main/java/com/example/app/controller/ReviewController.java
+++ src/main/java/com/example/app/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.app.domain.User;
 import com.example.app.service.ReviewService;
@@ -21,11 +22,15 @@ public class ReviewController {
 	@Autowired
 	HttpSession session;
 	
+	private static final int NUM_PER_PAGE = 5;
+	
 	@GetMapping
-	public String list(Model model) throws Exception{
+	public String list(@RequestParam(name = "page", defaultValue = "1") Integer page, Model model) throws Exception{
 		User user = (User) session.getAttribute("user");
 		model.addAttribute("user", user);
-		model.addAttribute("review", Rservice.getReviewList());
+		model.addAttribute("reviewList", Rservice.getReviewListByPage(page, NUM_PER_PAGE));
+		model.addAttribute("page", page);
+		model.addAttribute("totalPage", Rservice.getTotalPages(NUM_PER_PAGE));
 		return "review/list";
 	}
 }
