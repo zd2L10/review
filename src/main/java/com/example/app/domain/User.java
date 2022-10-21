14301187551eb73efeab39d9787b5c044d208da4
zd2L10 <airrideship556@gmail.com>
2022-10-21 09:25:38 +0900

    バリデーション実装

--- src/main/java/com/example/app/domain/User.java
+++ src/main/java/com/example/app/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.app.domain;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
@@ -7,11 +9,17 @@ public class User {
 
 	private Integer id;
 	
+	@NotBlank
 	private String loginId;
 	
+	@NotBlank
 	private String loginPass;
 	
 	private String name;
 	
 	private String role;
+	
+	private String confpass;
+	
+	private String login;
 }
